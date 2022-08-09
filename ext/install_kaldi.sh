f5d089bb6f798de332527e895625175e7e6263c1
Brian Wheeler <bdwheele@indiana.edu>
2022-08-09 11:20:55 -0400

    force a specific kaldi checkout

--- ext/install_kaldi.sh
+++ ext/install_kaldi.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# bdw: it looks like upstream has a particular checkout for the kaldi tree, so let's
+# try to use it.  Dan's repo used 7ffc9ddeb3c8436e16aece88364462c89672a183 and the
+# upstream switched to 498b25db122ec68a96aee154b9d829030adfae4c sometime in early
+# 2022.
+cd kaldi
+git checkout 498b25db122ec68a96aee154b9d829030adfae4c
+cd ..
+
 # Prepare Kaldi
 cd kaldi/tools
 #make clean
